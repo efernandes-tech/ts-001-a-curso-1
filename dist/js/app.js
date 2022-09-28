672de2b40c4ee2044657902d045f0be4cec8a8be
Éderson Fernandes <edersonluis.rf@gmail.com>
2022-09-27 21:58:04 -0300

    Instalando o TypeScript

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,4 +3,5 @@ import { Negociacao } from "./models/negociacao.js";
 // const negociacao = new Negociacao(new Date(), 10, 100);
 
 const negociacao = new Negociacao(new Date());
+
 console.log(negociacao.volume);
