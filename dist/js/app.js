019add5c4d3f5fb67985ad99c4b2e46289ab46b5
Éderson Fernandes <edersonluis.rf@gmail.com>
2022-09-27 21:43:04 -0300

    Motivação do TypeScript

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,6 @@
 import { Negociacao } from "./models/negociacao.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
+// const negociacao = new Negociacao(new Date(), 10, 100);
 
+const negociacao = new Negociacao(new Date());
 console.log(negociacao.volume);
