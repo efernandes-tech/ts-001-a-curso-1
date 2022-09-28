df7005db677e75e24ca31c43a158adc43d9e1ff1
Éderson Fernandes <edersonluis.rf@gmail.com>
2022-09-27 21:33:48 -0300

    Negociação, modelagem e regras

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,9 @@
-alert('teste')
+import { Negociacao } from "./models/negociacao.js";
+
+const negociacao = new Negociacao(new Date(), 10, 100);
+
+console.log(negociacao);
+
+negociacao.quantidade = 1000;
+
+console.log(negociacao);
