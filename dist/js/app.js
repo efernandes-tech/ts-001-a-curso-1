fd14d881bbde44d22b1f7bb5a74ae44d5d16b077
Éderson Fernandes <edersonluis.rf@gmail.com>
2022-09-27 21:38:49 -0300

    Finalização do modelo

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,8 +2,4 @@ import { Negociacao } from "./models/negociacao.js";
 
 const negociacao = new Negociacao(new Date(), 10, 100);
 
-console.log(negociacao);
-
-negociacao.quantidade = 1000;
-
-console.log(negociacao);
+console.log(negociacao.volume);
