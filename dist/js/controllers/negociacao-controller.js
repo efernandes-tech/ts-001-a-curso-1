3c1389e46394b6220e33f0842fe6498002af9b0b
Éderson Fernandes <edersonluis.rf@gmail.com>
2022-10-26 20:32:55 -0300

    Convertendo dados de entrada

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,12 +1,16 @@
 import { Negociacao } from "../models/negociacao.js";
 export class NegociacaoController {
     constructor() {
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
     }
     adiciona() {
-        const negociacao = new Negociacao(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ","));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        const negociacao = new Negociacao(date, quantidade, valor);
         console.log(negociacao);
     }
 }
