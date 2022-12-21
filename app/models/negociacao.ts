6d86294de6d949343a337f4c1a7bd21146da8caa
Éderson Fernandes <ederson.fernandes@bempromotora.onmicrosoft.com>
2022-12-21 10:34:40 -0300

    Organizando melhor nosso código

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,27 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
+  private _data: Date;
+  private _quantidade: number;
+  private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+  constructor(data: Date, quantidade: number, valor: number) {
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
+  }
 
-    get data() {
-        return this._data;
-    }
+  get data(): Date {
+    return this._data;
+  }
 
-    get quantidade() {
-        return this._quantidade;
-    }
+  get quantidade(): number {
+    return this._quantidade;
+  }
 
-    get valor() {
-        return this._valor;
-    }
+  get valor(): number {
+    return this._valor;
+  }
 
-    get volume() {
-        return this._quantidade * this._valor;
-    }
+  get volume(): number {
+    return this._quantidade * this._valor;
+  }
 }
