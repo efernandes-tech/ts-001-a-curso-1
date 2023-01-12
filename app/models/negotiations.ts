21b166ea05972beaa62b188e42733eeb42fc3af4
Éderson Fernandes <edersonluis.rf@gmail.com>
2023-01-11 22:20:28 -0300

    Moving forward with our model

--- app/models/negotiations.ts
+++ app/models/negotiations.ts
@@ -2,4 +2,12 @@ import { Negotiation } from "./negotiation.js";
 
 export class Negotiations {
   private negotiations: Array<Negotiation> = [];
+
+  add(negotiation: Negotiation) {
+    this.negotiations.push(negotiation);
+  }
+
+  list(): Array<Negotiation> {
+    return this.negotiations;
+  }
 }
