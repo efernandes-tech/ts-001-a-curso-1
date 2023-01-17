866a211cec539247139f88d90278bf51fdc323f4
Éderson Fernandes <edersonluis.rf@gmail.com>
2023-01-17 19:29:59 -0300

    More about readonly

--- app/models/negotiation.ts
+++ app/models/negotiation.ts
@@ -1,23 +1,11 @@
 export class Negotiation {
   constructor(
-    private _data: Date,
-    private _quantity: number,
-    private _value: number
+    public readonly data: Date,
+    public readonly quantity: number,
+    public readonly value: number
   ) {}
 
-  get data(): Date {
-    return this._data;
-  }
-
-  get quantity(): number {
-    return this._quantity;
-  }
-
-  get value(): number {
-    return this._value;
-  }
-
   get volume(): number {
-    return this._quantity * this._value;
+    return this.quantity * this.value;
   }
 }
