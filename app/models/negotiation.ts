5e9c23efdd180979f3baef0b9e693572cede19f4
Éderson Fernandes <edersonluis.rf@gmail.com>
2023-01-31 19:55:52 -0300

    defensive programming

--- app/models/negotiation.ts
+++ app/models/negotiation.ts
@@ -1,11 +1,16 @@
 export class Negotiation {
   constructor(
-    public readonly data: Date,
+    private _date: Date,
     public readonly quantity: number,
     public readonly value: number
   ) {}
 
   get volume(): number {
     return this.quantity * this.value;
+  }
+
+  get data(): Date {
+    const date = new Date(this._date.getTime());
+    return date;
   }
 }
